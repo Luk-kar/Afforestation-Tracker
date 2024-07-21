a5d7e19c5212f1e1c220c63283f36ee7aca796e6
Karol Lukaszczyk <lukkarcontact@gmail.com>
2024-07-21 15:46:52 +0200

    update

--- app/stages/data_acquisition.py
+++ app/stages/data_acquisition.py
@@ -1,146 +1,159 @@
-# Third-party imports
 import ee
 
-roi_coords = {
-    "roi_coords": [
-        [
-            [-17.5, 15.0],
-            [-17.5, 20.0],
-            [39.0, 20.0],
-            [43.0, 10.5],
-            [35.0, 8.0],
-            [25.0, 10.0],
-            [15.0, 8.0],
-            [-5.0, 12.0],
-            [-10.0, 13.0],
-        ]
-    ],
-    "start_date": "2020-01-01",
-    "end_date": "2020-12-31",
-}
 
-collections = {
-    # "sentinel2": "COPERNICUS/S2_HARMONIZED",
-    "surface_soil_moisture": "NASA/SMAP/SPL4SMGP/007",
-    "sm_rootzone": "NASA/SMAP/SPL4SMGP/007",
-    "elevation": "USGS/SRTMGL1_003",  # srtm
-    "precipitation": "UCSB-CHG/CHIRPS/PENTAD",
-    "soil_organic_carbon": "ISDASOIL/Africa/v1/carbon_total",
-    "world_cover": "ESA/WorldCover/v100/2020",
-}
+def get_rootzone_soil_moisture(roi_coords, start_date, end_date):
+    """
+    Retrieves the mean root zone soil moisture for a specified date range.
 
+    Parameters:
+        start_date (str): The start date of the period of interest in 'YYYY-MM-DD' format.
+        end_date (str): The end date of the period of interest in 'YYYY-MM-DD' format.
 
-def get_data():
-    data = download_data(roi_coords, collections)
-    return data
-
-
-def download_data(roi_dict, collections):
-    roi = ee.Geometry.Polygon(roi_dict["roi_coords"])
-    start_date = roi_dict["start_date"]
-    end_date = roi_dict["end_date"]
-
-    data = {}
-
-    # Sentinel-2 data for NDVI and SAVI
-    if "sentinel2" in collections:
-        data["sentinel2"] = get_sentinel2(collections, roi, start_date, end_date)
-
-    if "surface_soil_moisture" in collections:
-        data["surface_soil_moisture"] = get_surface_soil_moisture(
-            roi, start_date, end_date
-        )
-
-    # Rootzone Soil Moisture data
-    if "sm_rootzone" in collections:
-        data["sm_rootzone"] = get_rootzone_soil_moisture(roi, start_date, end_date)
-
-    # Elevation data (SRTM) for slope calculations
-    if "elevation" in collections:
-        elevation = get_elevation_data(collections["elevation"], roi)
-        data["elevation"] = elevation
-        data["slope"] = get_slope_data(elevation)
-
-    # Precipitation data (CHIRPS)
-    if "precipitation" in collections:
-        data["precipitation"] = get_precipitation_data(
-            collections["precipitation"], roi, start_date, end_date
-        )
-
-    # Soil Organic Carbon data
-    if "soil_organic_carbon" in collections:
-        data["soil_organic_carbon"] = get_soil_organic_carbon_data(
-            collections["soil_organic_carbon"], roi
-        )
-
-    # ESA WorldCover Data
-    if "world_cover" in collections:
-        data["world_cover"] = get_world_cover_data(collections["world_cover"], roi)
-
-    return data
-
-
-def get_surface_soil_moisture(roi, start_date, end_date):
+    Returns:
+        ee.Image: The mean root zone soil moisture image for the specified period.
+    """
     dataset = ee.ImageCollection("NASA/SMAP/SPL4SMGP/007")
+    roi = ee.Geometry.Polygon(roi_coords)
     filtered_dataset = dataset.filterDate(start_date, end_date).filterBounds(roi)
-    soil_moisture_surface = filtered_dataset.select("sm_surface").mean().clip(roi)
-    return soil_moisture_surface
+    soil_moisture_rootzone = filtered_dataset.select("sm_rootzone")
+    mean_soil_moisture_rootzone = soil_moisture_rootzone.mean().clip(roi)
+    return mean_soil_moisture_rootzone
 
 
-def get_rootzone_soil_moisture(roi, start_date, end_date):
-    dataset = ee.ImageCollection("NASA/SMAP/SPL4SMGP/007")
-    filtered_dataset = dataset.filterDate(start_date, end_date).filterBounds(roi)
-    soil_moisture_rootzone = filtered_dataset.select("sm_rootzone").mean().clip(roi)
-    return soil_moisture_rootzone
+def get_precipitation_data(roi_coords, start_date, end_date):
+    """
+    Retrieves the total precipitation for a specified date range.
 
+    Parameters:
+        roi_coords (list): List of coordinates defining the region of interest.
+        start_date (str): The start date of the period of interest in 'YYYY-MM-DD' format.
+        end_date (str): The end date of the period of interest in 'YYYY-MM-DD' format.
 
-def get_soil_organic_carbon_data(collection, roi):
+    Returns:
+        ee.Image: The total precipitation image for the specified period.
+    """
+    roi = ee.Geometry.Polygon(roi_coords)
     return (
-        ee.Image(collection).clip(roi).select("mean_0_20").rename("Soil_Organic_Carbon")
-    )
-
-
-def get_precipitation_data(collection, roi, start_date, end_date):
-    return (
-        ee.ImageCollection(collection)
+        ee.ImageCollection("UCSB-CHG/CHIRPS/DAILY")
         .filterDate(start_date, end_date)
         .filterBounds(roi)
+        .select("precipitation")
         .map(lambda image: image.clip(roi))  # Clip each image to the ROI
         .sum()
         .rename("Precipitation")
     )
 
 
-def get_slope_data(srtm):
-    return ee.Terrain.slope(srtm).rename("Slope")
+def get_elevation(roi_coords):
+    """
+    Retrieves the elevation data for the specified region of interest.
+
+    Parameters:
+        roi_coords (list): List of coordinates defining the region of interest.
+
+    Returns:
+        ee.Image: The elevation image for the specified region.
+    """
+    elevation = ee.Image("USGS/SRTMGL1_003")
+    roi = ee.Geometry.Polygon(roi_coords)
+    elevation_clipped = elevation.clip(roi)
+    return elevation_clipped
 
 
-def get_elevation_data(collection, roi):
-    return ee.Image(collection).clip(roi).rename("elevation")
+def get_slope(roi_coords):
+    """
+    Retrieves the slope data for the specified region of interest.
+
+    Parameters:
+        roi_coords (list): List of coordinates defining the region of interest.
+
+    Returns:
+        ee.Image: The slope image for the specified region.
+    """
+    elevation = get_elevation(roi_coords)
+    slope = ee.Terrain.slope(elevation)
+    return slope
 
 
-def get_soil_moisture(collection, roi, start_date, end_date):
-    return (
-        ee.ImageCollection(collection)
+def get_soil_organic_carbon(roi_coords):
+    """
+    Retrieves the soil organic carbon data for the specified region of interest.
+
+    Parameters:
+        roi_coords (list): List of coordinates defining the region of interest.
+
+    Returns:
+        ee.Image: The soil organic carbon image for the specified region.
+    """
+    soilGrids = ee.Image("ISDASOIL/Africa/v1/carbon_total")
+    soc_0_20cm = soilGrids.select("mean_0_20")
+    roi = ee.Geometry.Polygon(roi_coords)
+    soc_0_20cm_clipped = soc_0_20cm.clip(roi)
+    return soc_0_20cm_clipped
+
+
+def get_world_cover(roi_coords):
+    """
+    Retrieves the world cover data for the specified region of interest.
+
+    Parameters:
+        roi_coords (list): List of coordinates defining the region of interest.
+
+    Returns:
+        ee.Image: The world cover image for the specified region.
+    """
+    worldCover = ee.Image("ESA/WorldCover/v100/2020")
+    roi = ee.Geometry.Polygon(roi_coords)
+    worldCover_clipped = worldCover.clip(roi)
+    return worldCover_clipped
+
+
+def get_afforestation_candidates(roi_coords, start_date, end_date):
+    """
+    Identifies candidate regions for afforestation based on multiple criteria.
+
+    Parameters:
+        roi_coords (list): List of coordinates defining the region of interest.
+        start_date (str): The start date of the period of interest in 'YYYY-MM-DD' format.
+        end_date (str): The end date of the period of interest in 'YYYY-MM-DD' format.
+
+    Returns:
+        ee.Image: The candidate regions for afforestation.
+    """
+    roi = ee.Geometry.Polygon(roi_coords)
+
+    chirpsYear = (
+        ee.ImageCollection("UCSB-CHG/CHIRPS/DAILY")
         .filterDate(start_date, end_date)
         .filterBounds(roi)
-        .mean()
-        .select("sm_surface")
-        .rename("Soil_Moisture")
     )
+    annualPrecipitation = chirpsYear.sum()
+    elevation = get_elevation(roi_coords)
+    slope = ee.Terrain.slope(elevation)
 
-
-def get_sentinel2(collections, roi, start_date, end_date):
-    return (
-        ee.ImageCollection(collections["sentinel2"])
-        .filterDate(start_date, end_date)
+    filteredMoistureDataset = (
+        ee.ImageCollection("NASA/SMAP/SPL4SMGP/007")
+        .filterDate("2020-01-01", "2020-01-10")
         .filterBounds(roi)
-        .median()
     )
 
+    soilMoistureRootZone = filteredMoistureDataset.select("sm_rootzone")
+    suitableSlope = slope.lt(15)
+    soc = get_soil_organic_carbon(roi_coords)
+    suitableSOC = soc.gt(20)  # Example: suitable SOC greater than 20 g/kg
+    suitableMoisture = soilMoistureRootZone.mean().gte(
+        0.1
+    )  # Suitable soil moisture greater than or equal to 10% VWC
+    goodMoisture = suitableSOC.Or(suitableMoisture)
 
-def get_world_cover_data(collection, roi):
-    return ee.Image(collection).clip(roi)
+    candidateRegions = annualPrecipitation.gte(200).And(suitableSlope).And(goodMoisture)
 
+    worldCover = get_world_cover(roi_coords)
+    grassland = worldCover.eq(30)  # Grassland class in ESA WorldCover
+    barrenland = worldCover.eq(60)  # Barrenland class in ESA WorldCover
+    vegetationMask = grassland.Or(barrenland)
 
-# Data structure: data["sentinel2"], data["surface_soil_moisture"], data["sm_rootzone"], data["elevation"], data["slope"], data["precipitation"], data["soil_organic_carbon"], data["world_cover"]
+    # Apply the mask to the candidate regions
+    afforestationCandidates = candidateRegions.And(vegetationMask)
+
+    return afforestationCandidates
--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,60 +1,18 @@
-import streamlit as st
 import geemap.foliumap as geemap
-import ee
 
 # Initialize the Earth Engine module
 from stages.connection import establish_connection
+from stages.data_acquisition import (
+    get_rootzone_soil_moisture,
+    get_precipitation_data,
+    get_elevation,
+    get_slope,
+    get_soil_organic_carbon,
+    get_world_cover,
+    get_afforestation_candidates,
+)
 
 establish_connection()
-
-
-def get_rootzone_soil_moisture(roi_coords, start_date, end_date):
-    """
-    Retrieves the mean root zone soil moisture for a specified date range.
-
-    Parameters:
-        start_date (str): The start date of the period of interest in 'YYYY-MM-DD' format.
-        end_date (str): The end date of the period of interest in 'YYYY-MM-DD' format.
-
-    Returns:
-        ee.Image: The mean root zone soil moisture image for the specified period.
-    """
-
-    # Load the ImageCollection for SMAP Level 4 Global Daily 9 km EASE-Grid Soil Moisture
-    dataset = ee.ImageCollection("NASA/SMAP/SPL4SMGP/007")
-
-    roi = ee.Geometry.Polygon(roi_coords)
-    filtered_dataset = dataset.filterDate(start_date, end_date).filterBounds(roi)
-    soil_moisture_rootzone = filtered_dataset.select("sm_rootzone")
-    mean_soil_moisture_rootzone = soil_moisture_rootzone.mean().clip(roi)
-
-    return mean_soil_moisture_rootzone
-
-
-def get_precipitation_data(roi_coords, start_date, end_date):
-    """
-    Retrieves the total precipitation for a specified date range.
-
-    Parameters:
-        roi_coords (list): List of coordinates defining the region of interest.
-        start_date (str): The start date of the period of interest in 'YYYY-MM-DD' format.
-        end_date (str): The end date of the period of interest in 'YYYY-MM-DD' format.
-
-    Returns:
-        ee.Image: The total precipitation image for the specified period.
-    """
-
-    roi = ee.Geometry.Polygon(roi_coords)
-
-    return (
-        ee.ImageCollection("UCSB-CHG/CHIRPS/DAILY")
-        .filterDate(start_date, end_date)
-        .filterBounds(roi)
-        .select("precipitation")
-        .map(lambda image: image.clip(roi))  # Clip each image to the ROI
-        .sum()
-        .rename("Precipitation")
-    )
 
 
 def add_layer_to_map(Map, image, vis_params, layer_name):
@@ -72,8 +30,8 @@ roi_coords = [
     [-5.0, 12.0],
     [-10.0, 13.0],
 ]
-start_date = "2020-01-01"
-end_date = "2020-01-10"
+start_date = "2020-06-01"
+end_date = "2020-10-01"
 
 # Set visualization parameters for the soil moisture
 moisture_vis_params = {
@@ -99,12 +57,6 @@ precipitation_vis = {
         "FF0000",
     ],
 }
-
-# Additional datasets
-elevation = ee.Image("USGS/SRTMGL1_003")
-soilGrids = ee.Image("ISDASOIL/Africa/v1/carbon_total")
-soc_0_20cm = soilGrids.select("mean_0_20")
-worldCover = ee.Image("ESA/WorldCover/v100/2020")
 
 # Visualization parameters for additional layers
 elevation_vis = {
@@ -155,6 +107,12 @@ regions_vis = {
     ],
 }
 
+candidate_vis = {
+    "min": 0,
+    "max": 1,
+    "palette": ["red", "green"],
+}
+
 # Create a map centered on the specified location and zoom level
 Map = geemap.Map(center=(20, 0), zoom=2)
 
@@ -166,86 +124,26 @@ start_date = "2023-01-01"
 end_date = "2023-12-31"
 
 precipitation_map = get_precipitation_data(roi_coords, start_date, end_date)
-
-# Add the root zone soil moisture layer to the map
-# add_layer_to_map(
-#     Map, mean_soil_moisture_rootzone, moisture_vis_params, "Root Zone Soil Moisture"
-# )
-
-# Add additional layers to the map
-roi = ee.Geometry.Polygon(roi_coords)
-elevation_clipped = elevation.clip(roi)
-slope = ee.Terrain.slope(elevation_clipped)
-soc_0_20cm_clipped = soc_0_20cm.clip(roi)
-worldCover_clipped = worldCover.clip(roi)
-
-# add_layer_to_map(Map, elevation_clipped, elevation_vis, "Elevation")
-# add_layer_to_map(Map, slope, slope_vis, "Slope")
-# add_layer_to_map(Map, soc_0_20cm_clipped, soc_vis, "Soil Organic Carbon 0-20cm")
-# add_layer_to_map(Map, worldCover_clipped, regions_vis, "ESA WorldCover 2020")
-add_layer_to_map(Map, precipitation_map, precipitation_vis, "Precipitation")
-
-# Define the time range for one year (e.g., 2023)
-startDate = "2023-01-01"
-endDate = "2023-12-31"
-
-# Filter the CHIRPS data to the specified date range and region
-chirpsYear = (
-    ee.ImageCollection("UCSB-CHG/CHIRPS/DAILY")
-    .filterDate(startDate, endDate)
-    .filterBounds(roi)
+elevation_clipped = get_elevation(roi_coords)
+slope = get_slope(roi_coords)
+soc_0_20cm_clipped = get_soil_organic_carbon(roi_coords)
+worldCover_clipped = get_world_cover(roi_coords)
+afforestation_candidates = get_afforestation_candidates(
+    roi_coords, start_date, end_date
 )
 
-# Sum the daily precipitation to get the annual precipitation
-annualPrecipitation = chirpsYear.sum()
 
-# Calculate the slope in degrees
-slope = ee.Terrain.slope(elevation_clipped)
-
-# Define the time period of interest for moisture (modify dates as needed)
-filteredMoistureDataset = (
-    ee.ImageCollection("NASA/SMAP/SPL4SMGP/007")
-    .filterDate("2020-01-01", "2020-01-10")
-    .filterBounds(roi)
-)
-
-# Select the 'sm_surface' and 'sm_rootzone' bands
-soilMoistureSurface = filteredMoistureDataset.select("sm_surface")
-soilMoistureRootZone = filteredMoistureDataset.select("sm_rootzone")
-
-# Identify regions with minimal slope
-suitableSlope = slope.lt(15)
-
-# Identify regions with minimal soil organic carbon or sufficient moisture
-suitableSOC = soc_0_20cm_clipped.gt(20)  # Example: suitable SOC greater than 20 g/kg
-suitableMoisture = soilMoistureSurface.mean().gte(
-    0.1
-)  # Suitable soil moisture greater than or equal to 10% VWC
-goodMoisture = suitableSOC.Or(suitableMoisture)
-
-# Combine criteria to identify candidate regions
-candidateRegions = annualPrecipitation.gte(200).And(suitableSlope).And(goodMoisture)
-
-# Mask out areas that are not Grass or Shrubland or Barrenland
-grassland = worldCover_clipped.eq(30)  # Grassland class in ESA WorldCover
-barrenland = worldCover_clipped.eq(60)  # Barrenland class in ESA WorldCover
-vegetationMask = grassland.Or(barrenland)
-
-# Apply the mask to the candidate regions
-afforestationCandidates = candidateRegions.And(vegetationMask)
-
-# Visualization parameters for candidate regions
-candidateVis = {
-    "min": 0,
-    "max": 1,
-    "palette": ["red", "green"],
-}
-
-# Add the afforestation candidates layer to the map
 add_layer_to_map(
-    Map, afforestationCandidates, candidateVis, "Candidate Regions for Afforestation"
+    Map, mean_soil_moisture_rootzone, moisture_vis_params, "Root Zone Soil Moisture"
 )
-
+add_layer_to_map(Map, elevation_clipped, elevation_vis, "Elevation")
+add_layer_to_map(Map, slope, slope_vis, "Slope")
+add_layer_to_map(Map, soc_0_20cm_clipped, soc_vis, "Soil Organic Carbon 0-20cm")
+add_layer_to_map(Map, worldCover_clipped, regions_vis, "ESA WorldCover 2020")
+add_layer_to_map(Map, precipitation_map, precipitation_vis, "Precipitation")
+add_layer_to_map(
+    Map, afforestation_candidates, candidate_vis, "Candidate Regions for Afforestation"
+)
 
 # Display the map in Streamlit
 Map.to_streamlit(height=600)
