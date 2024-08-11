2821aadc937199b02bd97b401fa94057c579457b
Karol Lukaszczyk <lukkarcontact@gmail.com>
2024-08-11 17:37:56 +0200

    Testing propositions

---
+++ tests/test.py
@@ -0,0 +1,17 @@
+# test connections to Google Earth Engine
+
+# test collections and images from Google Earth Engine
+
+# test points returned from Google Earth Engine
+
+# test candidate for afforestation point and region
+
+# Test UI components
+
+# Test map visualization if is rendered
+
+# Test map point information formatting
+
+# Test coordinate input panel display
+
+# Test the legend display
