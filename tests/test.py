9ec1ed5fcb1008faab691ffd037ae8a0796959df
Karol Lukaszczyk <lukkarcontact@gmail.com>
2024-08-15 21:31:09 +0200

    `Refactor selenium imports for better code organization`

--- tests/test.py
+++ tests/test.py
@@ -17,9 +17,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.remote.webelement import WebElement
 
 # To avoid overloading the server
